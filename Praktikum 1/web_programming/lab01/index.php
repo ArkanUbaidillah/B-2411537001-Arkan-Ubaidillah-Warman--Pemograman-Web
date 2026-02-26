ad33122aa8b65d989439cfec4326c4cfef120f67
ARKT <arkanwarman@gmail.com>
2026-02-26 11:30:44 +0700

    Praktikum 1

---
+++ Praktikum 1/web_programming/lab01/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Praktikum Web Programming</title>
+</head>
+
+<body>
+    <h1>Hello World!</h1>
+    <?php
+    echo "<p>Ini adalah script PHP pertama saya</p>";
+
+    // Variabel
+    $name = "Arkan Ubaidillah Warman";
+    $npm = "2411537001";
+
+    // Output dengan concatenation
+    echo "<p>Nama saya: $name </p>";
+    echo "<p>NPM: " . $npm . "</p>";
+
+    // Fungsi date()
+    echo "<p>Hari ini: " . date("d-m-Y") . "</p>";
+    ?>
+</body>
+
+</html>
